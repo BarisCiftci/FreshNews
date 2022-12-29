fa9455c6755d42bf9ed230cabaac9f5de360f376
Baris Ciftci <barishciftci@gmail.com>
2022-12-29 21:10:09 +0100

    Initial Commit

---
+++ FreshNews/FreshNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshNewsApp.swift
+//  FreshNews
+//
+//  Created by Baris Ciftci on 29/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FreshNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
